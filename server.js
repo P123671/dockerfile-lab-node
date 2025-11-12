0fab840ccfa02b36a8a55ccc35d7799e03cf4cf4
P123671 <paul.khmaladze@gmail.com>
2025-11-12 13:24:43 +0000

    Added package and server files

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const MESSAGE = process.env.MESSAGE || "Hello from Docker!";
+
+app.get('/', (req, res) => {
+  res.send(`<h1>${MESSAGE}</h1>`);
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
